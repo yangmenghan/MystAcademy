244fe51ff2dc5e4f22947038f2a12e8242bb4b26
Menghan Yang <menghan.yang@360learning.com>
2018-05-29 23:47:18 +0200

    Add error display for login screen

--- app/src/main/java/com/mystacademy/android/MainActivity.kt
+++ app/src/main/java/com/mystacademy/android/MainActivity.kt
@@ -7,8 +7,6 @@ import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
 import com.mystacademy.android.LoginQueryMutation.Data
-import com.mystacademy.android.secrets.AppConstants
-import com.mystacademy.android.type.LoginInput
 import kotlinx.android.synthetic.main.activity_main.message
 import kotlinx.android.synthetic.main.activity_main.navigation
 
@@ -39,29 +37,8 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    testLoginQuery()
   }
 
-  private val apolloClient = ApolloClientManager.getClient()
-  private fun testLoginQuery() {
-    apolloClient.mutate(
-      LoginQueryMutation(
-        LoginInput.builder().username(AppConstants.TEST_USERNAME).password(
-          AppConstants.TEST_PWD
-        ).build()
-      )
-    )
-      .enqueue(testCallback)
-  }
-}
 
-object testCallback : ApolloCall.Callback<LoginQueryMutation.Data>() {
-  override fun onFailure(e: ApolloException) {
-    e.printStackTrace()
-  }
 
-  override fun onResponse(response: Response<Data>) {
-//    Timber.i(response.data().toString())
   }
-
-}
--- app/src/main/java/com/mystacademy/android/SplashActivity.kt
+++ app/src/main/java/com/mystacademy/android/SplashActivity.kt
@@ -1,10 +1,22 @@
 package com.mystacademy.android
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.constraint.Group
 import android.view.View
+import com.apollographql.apollo.ApolloCall.Callback
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.mystacademy.android.LoginQueryMutation.Data
+import com.mystacademy.android.R.string
+import com.mystacademy.android.type.LoginInput
+import kotlinx.android.synthetic.main.activity_splash.login_b
+import kotlinx.android.synthetic.main.activity_splash.login_password_et
+import kotlinx.android.synthetic.main.activity_splash.login_password_til
+import kotlinx.android.synthetic.main.activity_splash.login_userName_et
+import kotlinx.android.synthetic.main.activity_splash.login_userName_til
 
 class SplashActivity : Activity() {
 
@@ -12,6 +24,7 @@ class SplashActivity : Activity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_splash)
     window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    login_b.setOnClickListener { checkLoginInput() }
 
     // Route to main activity
     Handler().postDelayed({
@@ -26,5 +39,60 @@ class SplashActivity : Activity() {
     findViewById<Group>(R.id.login_group).visibility = View.VISIBLE
 
   }
-  //      startActivity(Intent(this, MainActivity::class.java))
+
+  private fun checkLoginInput() {
+    val username = login_userName_et.text.toString()
+    val password = login_password_et.text.toString()
+
+    if (username.isEmpty()) {
+      showUsernameEmptyError()
+    } else if (password.isEmpty()) {
+      showLoginEmptyError()
+    } else {
+      login(username, password)
+    }
+  }
+
+  private fun login(username: String, password: String) {
+    ApolloClientManager.getClient().mutate(
+      LoginQueryMutation(
+        LoginInput.builder().username(username).password(password).build()
+      )
+    ).enqueue(loginCallback)
+  }
+
+  private val loginCallback = object : Callback<Data>() {
+    override fun onFailure(e: ApolloException) {
+      e.printStackTrace()
+    }
+
+    override fun onResponse(response: Response<Data>) {
+      runOnUiThread {
+        if (!response.hasErrors()) {
+          routeToMain()
+        } else {
+          showLoginError()
+        }
+      }
+    }
+  }
+
+  private fun routeToMain() {
+    startActivity(Intent(this, MainActivity::class.java))
+  }
+
+  // view
+  private fun showUsernameEmptyError() {
+    login_userName_til.error = getString(string.error_login_username_empty)
+  }
+
+  private fun showLoginEmptyError() {
+    login_password_til.error = getString(string.error_login_password_empty)
+  }
+
+  private fun showLoginError() {
+    login_password_til.error = getString(string.error_login_failed)
+  }
+
+
 }
